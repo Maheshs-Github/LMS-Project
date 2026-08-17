688187f9e5d4e04a958e681c7baa48a9a8c4ef4d
Mahesh Mane <maheshmane9075@gmail.com>
2026-08-17 11:12:26 +0530

    changes of strted working on Course Managemnet and related functionality

--- server/models/course.model.js
+++ server/models/course.model.js
@@ -1,53 +1,63 @@
 import mongoose from "mongoose";
 
-const courseSchama=new mongoose.Schema({
-  title:{
-    required:true,
-    type:String,
+const courseSchama = new mongoose.Schema(
+  {
+    title: {
+      required: true,
+      type: String,
+    },
+    subTitle: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      required: true,
+      type: String,
+    },
+    price: {
+      required: true,
+      type: Number,
+    },
+    level: {
+      type: String,
+      enum: ["Beginner", "Moderate", "Advance"],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "rejected", "pending"],
+      required: true,
+      default: "pending",
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    enrolledStudents: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    lectures: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lecture",
+      },
+    ],
+    instructor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    isPublished: {
+      type: Boolean,
+      default: false,
+    },
   },
-  subTitle:{
-    type:String,
-  },
-  description:{
-    type:String,
-    required:true,
-  },
-  category:{
-    required:true,
-    type:String,
-  },
-    price:{
-    required:true,
-    type:Number,
-  },
-  level:{
-    type:String,
-    enum:["Beginner","Moderate","Advance"],
-    required:true,
-  },
-  thumbnail:{
-    type:String,
-    required:true,
-  },
-  enrolledStudents:[
-    {
-      type:mongoose.Schema.Types.ObjectId,
-      ref:"User",
-    }
-  ],
-  lectures:[{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Lecture"
-  }
-  ],
-  instructor:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User",
-  },
-  isPublished:{
-    type:Boolean,
-    default:false,
-  }
-},{timestamps:true});
+  { timestamps: true },
+);
 
-export const Course=mongoose.model("Course",courseSchama);
+export const Course = mongoose.model("Course", courseSchama);
